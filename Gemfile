524e69dee26cb0f9fc0d0dc51c3902e9f846d32a
Daniel Greenlaw <daniel@shirtspace.com>
2018-01-25 19:58:30 -0800

    Additional updates to content converter

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
 gem 'mysql2'
+gem 'nokogiri'
 gem 'puma', '~> 3.7'
 gem 'shopify_api'
 gem 'uglifier', '>= 1.3.0'
--- app/services/content_converter_service.rb
+++ app/services/content_converter_service.rb
@@ -8,6 +8,8 @@ class ContentConverterService
     converter.convert_captions
     converter.format_paragraphs
     converter.relink_images
+    converter.update_internal_links
+    converter.replace_domain_references
 
     converter.to_html
   end
@@ -17,8 +19,8 @@ class ContentConverterService
   end
 
   def convert_captions
-    @converted_content.gsub!(/\[caption.*?\].*\[\/caption\]$/) do |fragment|
-      content = fragment.match(/\[caption.*?\](.*)\[\/caption\]$/)[1]
+    @converted_content.gsub!(/\[caption.*?\].*\[\/caption\]/) do |fragment|
+      content = fragment.match(/\[caption.*?\](.*)\[\/caption\]/)[1]
       html = content.match(/(\<.*?\/.*\>)/)[1]
       text = content.match(/(.(?!>))+$/).to_s[1..-1].gsub(/\[(.*)$/, '').strip
       "<div class=\"image-with-caption\">#{html}<span>#{text}</span></div>"
@@ -26,16 +28,16 @@ class ContentConverterService
   end
 
   def embed_videos
-    @converted_content.gsub!(/http.*youtube\.com\/watch.*(?=\s|$)/) do |url|
-      token = url.match(/\?v=(.*)/)[1].strip
+    @converted_content.gsub!(/(?<!href=")((http.*youtu\.be\/|http.*youtube\.com\/watch\?v=)[a-zA-Z0-9_-]{11})/) do |url|
+      token = url.last(11)
       "<iframe width=\"640\" height=\"360\" src=\"https://www.youtube.com/embed/#{token}\" frameborder=\"0\" allowfullscreen></iframe>"
     end
   end
 
   def format_paragraphs
     c = @converted_content.strip.gsub(/\A[\n]*|[\n]*\Z/, '') # strip newlines at beginning and end of content
-    c = "#{c}</p>" unless c =~ /p>|div>\Z/ # if content doesn't end with block tag, add </p>
-    c = "<p>#{c}" unless c =~ /\A<p|<div/ # if content doesn't start with block tag, add <p>
+    c = "#{c}</p>" unless c =~ /p>\Z|div>\Z/ # if content doesn't end with block tag, add </p>
+    c = "<p>#{c}" unless c =~ /\A<p|\A<div/ # if content doesn't start with block tag, add <p>
     c.gsub!(/\n\t|(?<!\n)\n(?!\n)/, ' ') # strip single newlines and \n\t
     c.gsub!(/\n\n\s*\n\n(?=<\/div>)/, '') # \n\n \n\n</div> # remove two sets of newlines preceeding </div>
     c.gsub!(/\n\n(?=(<\/div>)|(<div>))|(?<=(<\/div>)|(<div>))\n\n/, '') # strip single \n\n preceeding or following <div> or </div>
@@ -43,12 +45,18 @@ class ContentConverterService
     c.gsub!(/(?<!>)\n\n(?=<p>)/, '</p>') # \n\n<p> replace \n\n with </p>
     c.gsub!(/[\n\n][\s*]*/, '</p><p>') # reduce one or more \n\n to </p><p>
 
-    @converted_content = c
+    # Now to do some DOM manipulation to deal with <div> city
+    doc = Nokogiri::HTML.parse c
+    doc.search('/html/body/div').each do |div|
+      # Just convert the top-level <div> to <p>
+      div.name = 'p'
+    end
+
+    @converted_content = doc.search('/html/body/*').map(&:to_html).join
   end
 
   def handle_images
     @converted_content.gsub!(/(\<img.*?\>)/) do |tag|
-      # has_class = tag =~ /class=".*size-[large|full].*?"/
       width = tag.match(/width="(\d*?)"/)[1].to_i
       klass = (width > 600) ? 'content-image--large' : ''
       klasses = " class=\"content-image #{klass}\""
@@ -62,10 +70,16 @@ class ContentConverterService
   end
 
   def relink_images
-    @converted_content.gsub!(/(?<=src=")http:\/\/www\.fishingaddictsnorthwest\.com\/wp-content\/uploads\S*(?=")/) do |url|
-      filename = url.split('/').last
-      # https://cdn.shopify.com/s/files/1/2372/4967/files/
-      "https://cdn.shopify.com/s/files/1/2541/0190/files/#{filename}"
+    @converted_content.gsub!(/(?<=src=")(http|https):\/\/w*\.*fishingaddictsnorthwest\.com\/wp-content\/uploads\S*(?=")/) do |url|
+      update_url(url)
+    end
+
+    @converted_content.gsub!(/\[embed\]|\[\/embed\]/, '')
+  end
+
+  def replace_domain_references
+    @converted_content.gsub!(/http(s*):\/\/w*\.*fishingaddictsnorthwest\.com\/*|w*\.*fishingaddictsnorthwest\.com\/*/) do |url|
+      'https://addicted.fishing'
     end
   end
 
@@ -86,6 +100,52 @@ class ContentConverterService
 
   def to_html
     @converted_content
+  end
+
+  def update_internal_links
+    doc = Nokogiri::HTML.parse @converted_content
+
+    doc.search('//a').each do |element|
+      href = element.attributes['href']
+
+      if href.present? && href.value.match(/fishingaddictsnorthwest\.com/)
+        url = href.value
+
+        new_href = if url.match(/uploads/).present?
+          # DownloadImagesService.single(url)
+          update_url(url)
+        elsif url.match(/com$|com\/$|tips-techniques\/bass-tips-techniques/).present?
+          '/'
+        elsif url.match(/p=88/).present?
+          "/blogs/news/learning-to-love-the-spawnbass-fishing"
+        elsif url.match(/columbia-river-guided-fishing/).present?
+          "/pages/columbia-river-guided-fishing-trip"
+        elsif url.match(/fishing-videos/).present?
+          "/pages/fishing-videos"
+        elsif url.match(/78141272/).present?
+          "/collections/addicted-landing-nets/products/addicted-cradle-net"
+        elsif url.match(/addicted-fishing-gear/).present?
+          "/collections/all"
+        else
+          handle = Post.where(post_name: url.match(/com\/(.*)\/$/)[1]).first&.shopify_handle
+          "/blogs/news/#{handle}" if handle
+        end
+
+        element.attributes['href'].value = new_href
+      end
+      # div.name = 'p'
+      byebug
+    end
+
+    @converted_content = doc.search('/html/body/*').map(&:to_html).join
+  end
+
+  def update_url(url)
+    filename = url.split('/').last
+    shop_id = Rails.application.secrets[:shopify_shop_id]
+    url_part = [shop_id.to_s[0..3], shop_id.to_s[4..-1]].join('/')
+
+    "https://cdn.shopify.com/s/files/1/#{url_part}/files/#{filename}"
   end
 
   def validate
